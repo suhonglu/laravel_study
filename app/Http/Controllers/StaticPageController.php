74328258bee8a409a09dbd3097064b2e5ac47bb2
honglu <330845113@qq.com>
2021-01-12 10:19:13 +0800

    first commit

---
+++ app/Http/Controllers/StaticPageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPageController extends Controller
+{
+    public function  home(){
+        return view('staticpages/home');
+    }
+}
---
+++ resources/views/staticpages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.default')
+@section('content')
+<a class="btn btn-lg btn-success" href="">现在注册</a>
+@stop
+
+@section('footer')
+<h3>第三个标题</h3>
+@stop
